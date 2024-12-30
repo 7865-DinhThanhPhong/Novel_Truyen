12230089b0e4d6902ab75e27f45d9662937b3520
7475_TrungHau <we.nobita1@gmail.com>
2024-12-30 23:03:00 +0700

    Backend Spring boot

---
+++ src/main/java/com/example/noveltruyen/NoveltruyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.noveltruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoveltruyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoveltruyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/noveltruyen/NoveltruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noveltruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoveltruyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
